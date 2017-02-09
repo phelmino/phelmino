7f9fea1c887689a985788594cd6e49ba447156b4
Matheus Cavalcante <matheus.cavalcante@ee.ufcg.edu.br>
2017-02-09 09:11:21 +0100

    Updating Library and .gitignore

--- id_stage.vhd
+++ id_stage.vhd
@@ -1,6 +1,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+library lib_VHDL;
+
 entity id_stage is
 
   port (
--- if_stage.vhd
+++ if_stage.vhd
@@ -1,6 +1,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+library lib_VHDL;
+
 entity if_stage is
 
   port (
