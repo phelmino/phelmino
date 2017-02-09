4c408e82e7dc4e734241c1bfe02cd396e3860952
Rafael Costa Sales <rafaelcsales@gmail.com>
2017-02-09 08:52:51 +0100

    Uploading phelmino_core.vhd

---
+++ phelmino_core.vhd
@@ -0,0 +1,11 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity phelmino_core is
+  
+  port (
+    -- Clock and reset signals
+    clk	  : in std_logic;
+    rst_n : in std_logic);
+
+end entity phelmino_core;
