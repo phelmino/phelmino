30ce2ee7d2c933e1d577fec30bae8a3601ff43a6
Matheus Cavalcante <matheus.cavalcante@ee.ufcg.edu.br>
2017-02-13 14:16:50 +0100

    Changement de l'interface du banc de registres

--- vhd/general_purpose_registers.vhd
+++ vhd/general_purpose_registers.vhd
@@ -15,19 +15,19 @@ entity general_purpose_registers is
     rst_n : in std_logic;
 
     -- Read interface
-    read_addr_a_en : in  std_logic;
-    read_addr_a_i  : in  std_logic_vector(N-1 downto 0);
-    read_data_a_o  : out std_logic_vector(W-1 downto 0);
+    read_enable_a_i : in  std_logic;
+    read_addr_a_i   : in  std_logic_vector(N-1 downto 0);
+    read_data_a_o   : out std_logic_vector(W-1 downto 0);
 
-    read_addr_b_en : in  std_logic;
-    read_addr_b_i  : in  std_logic_vector(N-1 downto 0);
-    read_data_b_o  : out std_logic_vector(W-1 downto 0);
+    read_enable_b_i : in  std_logic;
+    read_addr_b_i   : in  std_logic_vector(N-1 downto 0);
+    read_data_b_o   : out std_logic_vector(W-1 downto 0);
 
     -- Write interface
-    write_addr_a_en : in  std_logic;
-    write_addr_a_i  : in  std_logic_vector(N-1 downto 0);
-    write_data_a_i  : in  std_logic_vector(W-1 downto 0);
-    write_data_gnt  : out std_logic);
+    write_enable_a_i : in  std_logic;
+    write_addr_a_i   : in  std_logic_vector(N-1 downto 0);
+    write_data_a_i   : in  std_logic_vector(W-1 downto 0);
+    write_data_gnt_o : out std_logic);
 
 end entity general_purpose_registers;
 
@@ -47,9 +47,9 @@ begin  -- architecture behav
 
     if rst_n = '0' then                 -- asynchronous reset (active low)
       -- Clears outputs
-      read_data_a_o  <= (others => '0');
-      read_data_b_o  <= (others => '0');
-      write_data_gnt <= '0';
+      read_data_a_o    <= (others => '0');
+      read_data_b_o    <= (others => '0');
+      write_data_gnt_o <= '0';
 
       -- Clears register bank
       for i in 0 to 2**N-1 loop
@@ -57,23 +57,23 @@ begin  -- architecture behav
       end loop;
     elsif clk'event and clk = '1' then  -- rising clock edge
       -- Clears outputs
-      read_data_a_o  <= (others => '0');
-      read_data_b_o  <= (others => '0');
-      write_data_gnt <= '0';
+      read_data_a_o    <= (others => '0');
+      read_data_b_o    <= (others => '0');
+      write_data_gnt_o <= '0';
 
       -- Refreshes outputs
-      if (read_addr_a_en = '1') then
+      if (read_enable_a_i = '1') then
         read_data_a_o <= gpr(to_integer(unsigned(read_addr_a_i)));
       end if;
-      if (read_addr_b_en = '1') then
+      if (read_enable_b_i = '1') then
         read_data_b_o <= gpr(to_integer(unsigned(read_addr_b_i)));
       end if;
 
       -- Rewrites specific address in register bank, if not trying to read and
       -- write the same register
-      write_data_gnt <= next_write_data_gnt;
-      if (write_addr_a_en = '1' and next_write_data_gnt = '1') then
-        if (write_addr_a_i = "00000") then  -- Can not rewrite register r0
+      write_data_gnt_o <= next_write_data_gnt;
+      if (write_enable_a_i = '1' and next_write_data_gnt = '1') then
+        if (write_addr_a_i /= "00000") then  -- Can not rewrite register r0
           gpr(to_integer(unsigned(write_addr_a_i))) <= write_data_a_i;
         end if;
       end if;
@@ -85,13 +85,13 @@ begin  -- architecture behav
   -- type   : combinational
   -- inputs : read_addr_a_en, read_addr_a_i, read_addr_b_en, read_addr_b_i, write_addr_a_en, write_addr_a_i
   -- outputs: next_write_data_gnt
-  comb_proc : process (read_addr_a_en, read_addr_a_i, read_addr_b_en,
-                       read_addr_b_i, write_addr_a_en, write_addr_a_i) is
+  comb_proc : process (read_enable_a_i, read_addr_a_i, read_enable_b_i,
+                       read_addr_b_i, write_enable_a_i, write_addr_a_i) is
   begin  -- process comb_proc
     -- Default value
     next_write_data_gnt <= '0';
 
-    if (write_addr_a_en = '1') then
+    if (write_enable_a_i = '1') then
       next_write_data_gnt <= '1';
 
       if (write_addr_a_i = "00000") then
@@ -99,9 +99,9 @@ begin  -- architecture behav
         -- is accepted, even though the register
         -- will not be rewritten.
         next_write_data_gnt <= '1';
-      elsif (read_addr_a_en = '1' and read_addr_a_i = write_addr_a_i) then
+      elsif (read_enable_a_i = '1' and read_addr_a_i = write_addr_a_i) then
         next_write_data_gnt <= '0';
-      elsif (read_addr_b_en = '1' and read_addr_b_i = write_addr_a_i) then
+      elsif (read_enable_b_i = '1' and read_addr_b_i = write_addr_a_i) then
         next_write_data_gnt <= '0';
       end if;
 
