08374ed0ea4d297df240630ecde50048241a897c
Matheus Cavalcante <matheus.cavalcante@ee.ufcg.edu.br>
2017-02-16 10:23:38 +0100

    Nouveaux scripts de compilation

--- testbench/compil_bench.sh
+++ testbench/compil_bench.sh
@@ -1,5 +1,17 @@
 BENCH_PATH=`cd ../../; pwd`
 BENCH_LIB=lib_BENCH
+HAS_ERROR=0
+
+function compile {
+    if [ $HAS_ERROR = 0 ]; then
+        vcom -work ${BENCH_LIB} $1 > TMP
+        cat TMP
+        if [ `cat TMP | grep "** Error" | wc -l` != 0 ]; then
+            HAS_ERROR=1
+        fi
+        rm TMP
+    fi
+}
 
 if [ ! -e $BENCH_PATH ]; then
     mkdir -p $BENCH_PATH
@@ -11,6 +23,6 @@ vlib ${BENCH_PATH}/libs/${BENCH_LIB}
 
 vmap ${BENCH_LIB} ${BENCH_PATH}/libs/${BENCH_LIB}
 
-vcom -work ${BENCH_LIB} test_general_purpose_registers.vhd 
-vcom -work ${BENCH_LIB} test_if_stage.vhd 
-vcom -work ${BENCH_LIB} test_prefetch_buffer.vhd 
+compile test_general_purpose_registers.vhd
+compile test_if_stage.vhd
+compile test_prefetch_buffer.vhd 
