d6b14e47f6766a912ab93d4ffc27792b15ed8bbc
Matheus Cavalcante <matheus.cavalcante@ee.ufcg.edu.br>
2017-02-16 09:43:05 +0100

    Tests of Prefetch Buffer and GPR

--- testbench/test_general_purpose_registers.vhd
+++ testbench/test_general_purpose_registers.vhd
@@ -13,32 +13,32 @@ architecture behav of test_general_purpose_registers is
   component general_purpose_registers is
     generic (
       W : natural;
-      N : natural); 
+      N : natural);
     port (
-      clk              : in  std_logic;
-      rst_n            : in  std_logic;
-      read_enable_a_i  : in  std_logic;
-      read_addr_a_i    : in  std_logic_vector(N-1 downto 0);
-      read_data_a_o    : out std_logic_vector(W-1 downto 0);
-      read_enable_b_i  : in  std_logic;
-      read_addr_b_i    : in  std_logic_vector(N-1 downto 0);
-      read_data_b_o    : out std_logic_vector(W-1 downto 0);
-      write_enable_a_i : in  std_logic;
-      write_addr_a_i   : in  std_logic_vector(N-1 downto 0);
-      write_data_a_i   : in  std_logic_vector(W-1 downto 0)); 
+      CLK             : in  std_logic;
+      RST_n           : in  std_logic;
+      ReadEnableA_i   : in  std_logic;
+      ReadAddressA_i  : in  std_logic_vector(N-1 downto 0);
+      ReadDataA_o     : out std_logic_vector(W-1 downto 0);
+      ReadEnableB_i   : in  std_logic;
+      ReadAddressB_i  : in  std_logic_vector(N-1 downto 0);
+      ReadDataB_o     : out std_logic_vector(W-1 downto 0);
+      WriteEnableZ_i  : in  std_logic;
+      WriteAddressZ_i : in  std_logic_vector(N-1 downto 0);
+      WriteDataZ_i    : in  std_logic_vector(W-1 downto 0));
   end component general_purpose_registers;
 
-  signal clk              : std_logic                     := '0';
-  signal rst_n            : std_logic                     := '0';
-  signal read_enable_a_i  : std_logic                     := '0';
-  signal read_addr_a_i    : std_logic_vector(4 downto 0)  := (others => '0');
-  signal read_data_a_o    : std_logic_vector(31 downto 0);
-  signal read_enable_b_i  : std_logic                     := '0';
-  signal read_addr_b_i    : std_logic_vector(4 downto 0)  := (others => '0');
-  signal read_data_b_o    : std_logic_vector(31 downto 0);
-  signal write_enable_a_i : std_logic                     := '0';
-  signal write_addr_a_i   : std_logic_vector(4 downto 0)  := (others => '0');
-  signal write_data_a_i   : std_logic_vector(31 downto 0) := (others => '0');
+  signal CLK             : std_logic                     := '0';
+  signal RST_n           : std_logic                     := '0';
+  signal ReadEnableA_i   : std_logic                     := '0';
+  signal ReadAddressA_i  : std_logic_vector(4 downto 0)  := (others => '0');
+  signal ReadDataA_o     : std_logic_vector(31 downto 0);
+  signal ReadEnableB_i   : std_logic                     := '0';
+  signal ReadAddressB_i  : std_logic_vector(4 downto 0)  := (others => '0');
+  signal ReadDataB_o     : std_logic_vector(31 downto 0);
+  signal WriteEnableZ_i  : std_logic                     := '0';
+  signal WriteAddressZ_i : std_logic_vector(4 downto 0)  := (others => '0');
+  signal WriteDataZ_i    : std_logic_vector(31 downto 0) := (others => '0');
 
 begin  -- architecture behav
 
@@ -47,84 +47,109 @@ begin  -- architecture behav
       W => 32,
       N => 5)
     port map (
-      clk              => clk,
-      rst_n            => rst_n,
-      read_enable_a_i  => read_enable_a_i,
-      read_addr_a_i    => read_addr_a_i,
-      read_data_a_o    => read_data_a_o,
-      read_enable_b_i  => read_enable_b_i,
-      read_addr_b_i    => read_addr_b_i,
-      read_data_b_o    => read_data_b_o,
-      write_enable_a_i => write_enable_a_i,
-      write_addr_a_i   => write_addr_a_i,
-      write_data_a_i   => write_data_a_i);
+      CLK             => CLK,
+      RST_n           => RST_n,
+      ReadEnableA_i   => ReadEnableA_i,
+      ReadAddressA_i  => ReadAddressA_i,
+      ReadDataA_o     => ReadDataA_o,
+      ReadEnableB_i   => ReadEnableB_i,
+      ReadAddressB_i  => ReadAddressB_i,
+      ReadDataB_o     => ReadDataB_o,
+      WriteEnableZ_i  => WriteEnableZ_i,
+      WriteAddressZ_i => WriteAddressZ_i,
+      WriteDataZ_i    => WriteDataZ_i);
 
-  clk   <= not clk after 5 ns;
-  rst_n <= '1'     after 7 ns;
+  CLK   <= not CLK after 5 ns;
+  RST_n <= '1'     after 7 ns;
 
-  stimulus : process is
-  begin  -- process stimulus
-    -- Intention to write 
-    write_enable_a_i <= '1';
+  Stimulus : process is
+    variable ReadCounterA  : std_logic_vector(31 downto 0) := (0      => '1', others => '0');
+    variable ReadAddressA  : std_logic_vector(4 downto 0)  := (others => '0');
+    variable ReadCounterB  : std_logic_vector(31 downto 0) := (0      => '1', others => '0');
+    variable ReadAddressB  : std_logic_vector(4 downto 0)  := (others => '0');
+    variable WriteCounterZ : std_logic_vector(31 downto 0) := (others => '0');
+    variable WriteAddressZ : std_logic_vector(4 downto 0)  := (others => '0');
+  begin  -- process Stimulus
 
-    -- Writes in some registers
-    write_addr_a_i <= "00000";
-    write_data_a_i <= (0 => '0', others => '1');
-    wait on clk;
-    wait on clk;
+    WriteEnableZ_i <= '0';
+    ReadEnableA_i  <= '0';
+    ReadEnableB_i  <= '0';
+    wait for 10 ns;
+    wait until falling_edge(CLK);
 
-    write_addr_a_i <= "00001";
-    write_data_a_i <= (1 => '0', others => '1');
-    wait on clk;
-    wait on clk;
+    for W in 0 to 31 loop
+      WriteEnableZ_i  <= '1';
+      WriteAddressZ_i <= WriteAddressZ;
+      WriteDataZ_i    <= WriteCounterZ;
+      WriteCounterZ   := std_logic_vector(unsigned(WriteCounterZ) + 1);
+      WriteAddressZ   := std_logic_vector(unsigned(WriteAddressZ) + 1);
+      wait until falling_edge(CLK);
+    end loop;  -- W
 
-    write_addr_a_i <= "00010";
-    write_data_a_i <= (2 => '0', others => '1');
-    wait on clk;
-    wait on clk;
+    WriteEnableZ_i <= '0';
+    wait until falling_edge(CLK);
 
-    write_addr_a_i <= "00011";
-    write_data_a_i <= (3 => '0', others => '1');
-    wait on clk;
-    wait on clk;
+    for RA in 0 to 31 loop
+      ReadEnableA_i  <= '1';
+      ReadAddressA_i <= ReadAddressA;
+      wait until falling_edge(CLK);
 
-    -- No more intention to write
-    write_enable_a_i <= '0';
-    wait on clk;
-    wait on clk;
+      if (RA = 0) then
+        assert (unsigned(ReadDataA_o) = 0) report "Can not write over register 0" severity failure;
+      else
+        assert (ReadDataA_o = std_logic_vector(unsigned(ReadCounterA))) report "Read error in register A" severity failure;
+        ReadCounterA := std_logic_vector(unsigned(ReadCounterA) + 1);
+      end if;
+      ReadAddressA := std_logic_vector(unsigned(ReadAddressA) + 1);
+      wait until falling_edge(CLK);
+    end loop;  -- RA
+    ReadEnableA_i <= '0';
 
-    read_enable_a_i <= '1';
-    read_addr_a_i   <= "00000";
-    read_enable_b_i <= '1';
-    read_addr_b_i   <= "00001";
-    wait on clk;
-    wait on clk;
+    wait for 10 ns;
 
-    assert (unsigned(read_data_a_o) = 0) report "Can not write over register r0" severity warning;
-    assert (read_data_b_o(3 downto 0) = "1101") report "Could not read data in register r1" severity warning;
-    read_enable_a_i <= '0';
-    read_addr_b_i   <= "00010";
-    wait on clk;
-    wait on clk;
+    for RB in 0 to 31 loop
+      ReadEnableB_i  <= '1';
+      ReadAddressB_i <= ReadAddressB;
+      wait until falling_edge(CLK);
 
-    assert (unsigned(read_data_a_o) = 0) report "Data left in disabled read port" severity warning;
-    assert (read_data_b_o(3 downto 0) = "1011") report "Could not read data in register r2" severity warning;
-    wait on clk;
-    wait on clk;
+      if (RB = 0) then
+        assert (unsigned(ReadDataB_o) = 0) report "Can not write over register 0" severity failure;
+      else
+        assert (ReadDataB_o = std_logic_vector(unsigned(ReadCounterB))) report "Read error in register B" severity failure;
+        ReadCounterB := std_logic_vector(unsigned(ReadCounterB) + 1);
+      end if;
+      ReadAddressB := std_logic_vector(unsigned(ReadAddressB) + 1);
+      wait until falling_edge(CLK);
+    end loop;  -- RB
+    ReadEnableB_i <= '0';
 
-    write_enable_a_i <= '1';
-    write_addr_a_i   <= "00010";
-    write_data_a_i   <= (others => '1');
-    wait on clk;
-    wait on clk;
+    wait for 10 ns;
 
-    assert (read_data_b_o(3 downto 0) = "1011") report "Incorrect data in register r2" severity warning;
-    wait on clk;
-    wait on clk;
+    ReadCounterA := (0      => '1', others => '0');
+    ReadAddressA := (others => '0');
+    ReadCounterB := (0      => '1', others => '0');
+    ReadAddressB := (others => '0');
 
-    assert (read_data_b_o(3 downto 0) = "1111") report "Incorrect data in register r2" severity warning;
+    for RC in 0 to 31 loop
+      ReadEnableA_i  <= '1';
+      ReadEnableB_i  <= '1';
+      ReadAddressA_i <= ReadAddressA;
+      ReadAddressB_i <= ReadAddressB;
+      wait until falling_edge(CLK);
 
-    wait;
-  end process stimulus;
+      if (RC = 0) then
+        assert (unsigned(ReadDataA_o) = 0) report "Can not write over register 0" severity failure;
+        assert (unsigned(ReadDataB_o) = 0) report "Can not write over register 0" severity failure;
+      else
+        assert (ReadDataA_o = std_logic_vector(unsigned(ReadCounterA))) report "Read error in register A" severity failure;
+        assert (ReadDataB_o = std_logic_vector(unsigned(ReadCounterB))) report "Read error in register B" severity failure;
+        ReadCounterA := std_logic_vector(unsigned(ReadCounterA) + 1);
+        ReadCounterB := std_logic_vector(unsigned(ReadCounterB) + 1);
+      end if;
+      ReadAddressA := std_logic_vector(unsigned(ReadAddressA) + 1);
+      ReadAddressB := std_logic_vector(unsigned(ReadAddressB) + 1);
+      wait until falling_edge(CLK);
+    end loop;  -- RC
+  end process Stimulus;
 
 end architecture behav;
--- vhd/prefetch_buffer.vhd
+++ vhd/prefetch_buffer.vhd
@@ -11,14 +11,14 @@ entity Prefetch_Buffer is
     DATA_WIDTH : natural := 32);
 
   port (
-    CLK     : in  std_logic;
-    RST_n   : in  std_logic;
-    WriteEn : in  std_logic;
-    DataIn  : in  std_logic_vector(DATA_WIDTH-1 downto 0);
-    ReadEn  : in  std_logic;
-    DataOut : out std_logic_vector(DATA_WIDTH-1 downto 0);
-    Empty   : out std_logic;
-    Full    : out std_logic);
+    CLK          : in  std_logic;
+    RST_n        : in  std_logic;
+    Write_Enable : in  std_logic;
+    Data_Input   : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+    Read_Enable  : in  std_logic;
+    Data_Output  : out std_logic_vector(DATA_WIDTH-1 downto 0);
+    Empty        : out std_logic;
+    Full         : out std_logic);
 
 end entity Prefetch_Buffer;
 
@@ -40,28 +40,28 @@ begin  -- architecture Behavioural
   -- Full if number of writes is equal to the number of reads - DEPTH
   Full  <= '1' when (unsigned(StatusCounter) = DEPTH) else '0';
 
-  -- purpose: Updates DataOut
+  -- purpose: Updates Data_Output
   -- type   : sequential
-  -- inputs : CLK, RST_n, ReadEn
-  -- outputs: DataOut
+  -- inputs : CLK, RST_n, Read_Enable
+  -- outputs: Data_Output
   ReadProc : process (CLK, RST_n) is
   begin  -- process ReadProc
     if RST_n = '0' then                 -- asynchronous reset (active low)
-      DataOut     <= (others => '0');
+      Data_Output <= (others => '0');
       ReadPointer <= (others => '0');
     elsif CLK'event and CLK = '1' then  -- rising clock edge
-      if (ReadEn = '1' and (unsigned(StatusCounter) /= 0)) then
-        DataOut     <= FIFO(to_integer(unsigned(ReadPointer)));
+      if (Read_Enable = '1' and (unsigned(StatusCounter) /= 0)) then
+        Data_Output <= FIFO(to_integer(unsigned(ReadPointer)));
         ReadPointer <= std_logic_vector(unsigned(ReadPointer) + 1);
       else
-        DataOut <= (others => '0');
+        Data_Output <= (others => '0');
       end if;
     end if;
   end process ReadProc;
 
-  -- purpose: Reads DataIn
+  -- purpose: Reads Data_Input
   -- type   : sequential
-  -- inputs : CLK, RST_n, WriteEn, DataIn
+  -- inputs : CLK, RST_n, Write_Enable, Data_Input
   -- outputs: 
   WriteProc : process (CLK, RST_n) is
   begin  -- process WriteProc
@@ -71,8 +71,8 @@ begin  -- architecture Behavioural
         FIFO(I) <= (others => '0');
       end loop;  -- I
     elsif CLK'event and CLK = '1' then  -- rising clock edge
-      if ((WriteEn = '1') and (unsigned(StatusCounter) /= DEPTH)) then
-        FIFO(to_integer(unsigned(WritePointer))) <= DataIn;
+      if ((Write_Enable = '1') and (unsigned(StatusCounter) /= DEPTH)) then
+        FIFO(to_integer(unsigned(WritePointer))) <= Data_Input;
         WritePointer                             <= std_logic_vector(unsigned(WritePointer) + 1);
       end if;
     end if;
@@ -80,18 +80,18 @@ begin  -- architecture Behavioural
 
   -- purpose: Update Status Counter
   -- type   : sequential
-  -- inputs : CLK, RST_n, ReadEn, WriteEn
+  -- inputs : CLK, RST_n, Read_Enable, Write_Enable
   -- outputs: StatusCounter
   StatusCounterProc : process (CLK, RST_n) is
   begin  -- process StatusCounterProc
     if RST_n = '0' then                 -- asynchronous reset (active low)
       StatusCounter <= (others => '0');
     elsif CLK'event and CLK = '1' then  -- rising clock edge
-      if ((ReadEn = '1') and (WriteEn = '0') and (unsigned(StatusCounter) /= 0)) then  -- Only reading
+      if ((Read_Enable = '1') and (Write_Enable = '0') and (unsigned(StatusCounter) /= 0)) then  -- Only reading
         StatusCounter <= std_logic_vector(unsigned(StatusCounter) - 1);
-      elsif ((ReadEn = '0') and (WriteEn = '1') and (unsigned(StatusCounter) /= DEPTH)) then  -- Only writing
+      elsif ((Read_Enable = '0') and (Write_Enable = '1') and (unsigned(StatusCounter) /= DEPTH)) then  -- Only writing
         StatusCounter <= std_logic_vector(unsigned(StatusCounter) + 1);
-      elsif ((ReadEn = '1') and (WriteEn = '1') and (unsigned(StatusCounter) = DEPTH)) then  -- Trying to read and write when full : only does the read operation.
+      elsif ((Read_Enable = '1') and (Write_Enable = '1') and (unsigned(StatusCounter) = DEPTH)) then  -- Trying to read and write when full : only does the read operation.
         StatusCounter <= std_logic_vector(unsigned(StatusCounter) - 1);
       end if;
     end if;
