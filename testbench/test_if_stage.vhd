1c16ce5ce5539f13971e67b22474e3f4673a7630
Matheus Cavalcante <matheus.cavalcante@ee.ufcg.edu.br>
2017-02-28 23:04:36 +0100

    Signaux de branchement

--- testbench/test_if_stage.vhd
+++ testbench/test_if_stage.vhd
@@ -21,7 +21,9 @@ architecture Behavioural of test_if_stage is
       Instr_ReqValid_Input     : in  std_logic;
       Instr_ReqData_Input      : in  std_logic_vector(WORD_WIDTH-1 downto 0);
       Instr_ReqValid_ID_Output : out std_logic;
-      Instr_ReqData_ID_Output  : out std_logic_vector(WORD_WIDTH-1 downto 0));
+      Instr_ReqData_ID_Output  : out std_logic_vector(WORD_WIDTH-1 downto 0);
+      Branch_Active_Input      : in  std_logic;
+      Branch_Destination_Input : in  std_logic_vector(WORD_WIDTH-1 downto 0));
   end component IF_Stage;
 
   signal CLK                        : std_logic                               := '0';
@@ -34,6 +36,8 @@ architecture Behavioural of test_if_stage is
   signal Instr_ReqValid_ID_Output   : std_logic                               := '0';
   signal Instr_ReqData_ID_Output    : std_logic_vector(WORD_WIDTH-1 downto 0) := (others => '0');
   signal Next_Grant                 : std_logic                               := '0';
+  signal Branch_Active_Input        : std_logic                               := '0';
+  signal Branch_Destination_Input   : std_logic_vector(WORD_WIDTH-1 downto 0) := (others => '0');
 
 begin  -- architecture Behavioural
 
@@ -51,20 +55,33 @@ begin  -- architecture Behavioural
       Instr_ReqValid_Input     => Instruction_ReqValid_Input,
       Instr_ReqData_Input      => Instr_ReqData_Input,
       Instr_ReqValid_ID_Output => Instr_ReqValid_ID_Output,
-      Instr_ReqData_ID_Output  => Instr_ReqData_ID_Output);
+      Instr_ReqData_ID_Output  => Instr_ReqData_ID_Output,
+      Branch_Active_Input      => Branch_Active_Input,
+      Branch_Destination_Input => Branch_Destination_Input);
 
   -- purpose: Emulate the memory
   Proc_Memory : process (CLK, RST_n) is
+    variable Counter : unsigned(WORD_WIDTH-1 downto 0) := (others => '0');
   begin  -- process proc_memory
     if RST_n = '0' then                 -- asynchronous reset (active low)
       Instr_Grant_Input          <= '0';
       Instr_ReqData_Input        <= (others => '0');
       Instruction_ReqValid_Input <= '0';
+    elsif CLK'event and CLK = '1' then  -- rising clock edge
+      Branch_Active_Input      <= '0';
+      Branch_Destination_Input <= (others => '0');
+      if (Instr_Grant_Input = '1') then
+        Counter := Counter + 1;
+        if (Counter = 3) then
+          Branch_Active_Input      <= '1';
+          Branch_Destination_Input <= (1 => '1', 3 => '1', others => '0');
+        end if;
+      end if;
     elsif CLK'event and CLK = '0' then  -- falling clock edge
       if (Instr_Grant_Input = '1') then
         Instr_Grant_Input          <= '0';
         Instruction_ReqValid_Input <= '1';
-        Instr_ReqData_Input     <= "00000000001000001000000110110011";
+        Instr_ReqData_Input        <= std_logic_vector(Counter);
       elsif (Instruction_ReqValid_Input = '1') then
         Instruction_ReqValid_Input <= '0';
         Instr_ReqData_Input        <= (others => '0');
