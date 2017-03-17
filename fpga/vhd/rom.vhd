ef6846cb3cee3de70ba09c479847f52b8a348965
Matheus Cavalcante <matheus.cavalcante@ee.ufcg.edu.br>
2017-03-17 15:33:31 +0100

    Nouveaux fichers

--- fpga/vhd/rom.vhd
+++ fpga/vhd/rom.vhd
@@ -9,9 +9,41 @@ library lib_fpga;
 use lib_fpga.memory_definitions.all;
 
 entity rom is
-  
+
   generic (
-    rom_depth  : natural := ROM_DEPTH;
-    word_width : natural := MEMORY_WIDTH);
+    depth : natural := MEMORY_DEPTH;
+    width : natural := MEMORY_WIDTH);
+
+  port (
+    clk     : in  std_logic;
+    rst_n   : in  std_logic;
+    address : in  std_logic_vector(depth-1 downto 0);
+    output  : out std_logic_vector(width-1 downto 0));
 
 end entity rom;
+
+architecture behavioural of rom is
+
+  type rom_data_type is array (0 to ROM_DEPTH) of std_logic_vector(width-1 downto 0);
+  constant rom_data : rom_data_type :=
+    (others => x"FFFF");
+
+  signal next_output : std_logic_vector(width-1 downto 0);
+
+begin  -- architecture behavioural
+
+  sequential : process (clk, rst_n) is
+  begin  -- process sequential
+    if rst_n = '0' then                 -- asynchronous reset (active low)
+      output <= (others => '0');
+    elsif clk'event and clk = '1' then  -- rising clock edge
+      output <= next_output;
+    end if;
+  end process sequential;
+
+  combinational : process (address) is
+  begin  -- process combinational
+    next_output <= rom_data(to_integer(unsigned(address)));
+  end process combinational;
+
+end architecture behavioural;
---
+++ fpga/vhd/seven_segments.vhd
@@ -0,0 +1,45 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
+
+library lib_fpga;
+use lib_fpga.memory_definitions.all;
+
+library lib_vhdl;
+use lib_vhdl.phelmino_definitions.all;
+
+entity rom_digit is
+
+  port (
+    digit  : in  std_logic_vector(3 downto 0);
+    output : out std_logic_vector(6 downto 0));
+
+end entity rom_digit;
+
+architecture behavioural of rom_digit is
+
+  type tab_7seg is array (0 to 15) of bit_vector(7 downto 0);
+  constant conversion_table : tab_7seg :=
+    (0  => x"40",
+     1  => x"79",
+     2  => x"24",
+     3  => x"30",
+     4  => x"19",
+     5  => x"12",
+     6  => x"02",
+     7  => x"78",
+     8  => x"00",
+     9  => x"10",
+     10 => x"08",
+     11 => x"03",
+     12 => x"46",
+     13 => x"21",
+     14 => x"06",
+     15 => x"0E");
+
+begin  -- architecture behavioural
+
+  output <= to_stdlogicvector(conversion_table(conv_integer(digit))(6 downto 0));
+
+end architecture behavioural;
