6240d12ce079bfba4f69f91b072837667e88952a
Matheus Cavalcante <matheus.cavalcante@ee.ufcg.edu.br>
2017-03-19 10:10:52 +0100

    Trying to treat the hazards

--- vhd/ex_stage.vhd
+++ vhd/ex_stage.vhd
@@ -64,29 +64,29 @@ architecture behavioural of ex_stage is
   signal next_branch_active           : std_logic;
   signal next_destination_register_wb : std_logic_vector(GPR_ADDRESS_WIDTH-1 downto 0);
 
+  signal data_requisition_i     : std_logic;
+  signal data_address_i         : std_logic_vector(WORD_WIDTH-1 downto 0);
+  signal data_write_enable_i    : std_logic;
+  signal data_write_data_i      : std_logic_vector(WORD_WIDTH-1 downto 0);
   signal last_data_requisition  : std_logic;
   signal last_data_address      : std_logic_vector(WORD_WIDTH-1 downto 0);
   signal last_data_write_enable : std_logic;
   signal last_data_write_data   : std_logic_vector(WORD_WIDTH-1 downto 0);
 
-  signal next_data_requisition     : std_logic;
-  signal next_data_address         : std_logic_vector(WORD_WIDTH-1 downto 0);
-  signal next_data_write_enable    : std_logic;
-  signal next_data_write_data      : std_logic_vector(WORD_WIDTH-1 downto 0);
-  signal waiting_for_memory        : std_logic;
-  signal next_waiting_for_memory   : std_logic;
-  signal current_is_requisition_wb : std_logic;
-  signal next_is_requisition_wb    : std_logic;
+  signal waiting_for_memory      : std_logic;
+  signal next_waiting_for_memory : std_logic;
+  signal is_requisition_wb_i     : std_logic;
+  signal next_is_requisition_wb  : std_logic;
 
 begin  -- architecture behavioural
 
   ready_id         <= ready when (waiting_for_memory = '0') else ready and data_grant;
+  alu_result_id    <= alu_result;
   branch_active_if <= branch_active;
   branch_active_id <= branch_active;
-  alu_result_id    <= alu_result;
 
   -- gpr
-  write_enable_z_id  <= not next_data_requisition;
+  write_enable_z_id  <= not data_requisition_i;
   write_address_z_id <= destination_register;
   write_data_z_id    <= alu_result;
 
@@ -106,13 +106,16 @@ begin  -- architecture behavioural
       -- branch
       branch_active <= '0';
 
-      last_data_requisition     <= '0';
-      last_data_address         <= (others => '0');
-      last_data_write_enable    <= '0';
-      last_data_write_data      <= (others => '0');
-      waiting_for_memory        <= '0';
-      current_is_requisition_wb <= '0';
-      is_requisition_wb         <= '0';
+      -- memorizing signals from memory
+      last_data_requisition  <= '0';
+      last_data_address      <= (others => '0');
+      last_data_write_enable <= '0';
+      last_data_write_data   <= (others => '0');
+      waiting_for_memory     <= '0';
+
+      -- interface with wb stage
+      is_requisition_wb   <= '0';
+      is_requisition_wb_i <= '0';
 
     elsif clk'event and clk = '1' then  -- rising clock edge
       -- destination register
@@ -121,23 +124,26 @@ begin  -- architecture behavioural
       -- branch
       branch_active <= next_branch_active;
 
-      last_data_requisition     <= next_data_requisition;
-      last_data_address         <= next_data_address;
-      last_data_write_enable    <= next_data_write_enable;
-      last_data_write_data      <= next_data_write_data;
-      waiting_for_memory        <= next_waiting_for_memory;
-      current_is_requisition_wb <= next_is_requisition_wb;
-      is_requisition_wb         <= next_is_requisition_wb;
+      -- memorizing signals from memory
+      last_data_requisition  <= data_requisition_i;
+      last_data_address      <= data_address_i;
+      last_data_write_enable <= data_write_enable_i;
+      last_data_write_data   <= data_write_data_i;
+      waiting_for_memory     <= next_waiting_for_memory;
+
+      -- interface with wb stage
+      is_requisition_wb_i <= next_is_requisition_wb;
+      is_requisition_wb   <= next_is_requisition_wb;
 
     end if;
   end process sequential;
 
-  combinational : process (alu_result, branch_active,
-                           current_is_requisition_wb, data_grant,
+  combinational : process (alu_result, branch_active, data_grant,
                            destination_register, is_branch, is_requisition,
-                           is_write, is_write_data, last_data_address,
-                           last_data_requisition, last_data_write_data,
-                           last_data_write_enable, ready, waiting_for_memory)
+                           is_requisition_wb_i, is_write, is_write_data,
+                           last_data_address, last_data_requisition,
+                           last_data_write_data, last_data_write_enable, ready,
+                           waiting_for_memory)
   begin  -- process combinational
 
     case branch_active is
@@ -146,8 +152,6 @@ begin  -- architecture behavioural
         next_branch_active           <= '0';
 
       when others =>
-        -- TODO: add 'and data_grant'? or make
-        -- destination_register_wb <= destination_register?
         next_destination_register_wb <= destination_register;
         next_branch_active           <= is_branch and alu_result(0);
     end case;
@@ -158,48 +162,53 @@ begin  -- architecture behavioural
         data_address            <= last_data_address;
         data_write_enable       <= last_data_write_enable;
         data_write_data         <= last_data_write_data;
-        next_data_requisition   <= last_data_requisition;
-        next_data_address       <= last_data_address;
-        next_data_write_enable  <= last_data_write_enable;
-        next_data_write_data    <= last_data_write_data;
+        data_requisition_i      <= last_data_requisition;
+        data_address_i          <= last_data_address;
+        data_write_enable_i     <= last_data_write_enable;
+        data_write_data_i       <= last_data_write_data;
         next_waiting_for_memory <= waiting_for_memory;
-        next_is_requisition_wb  <= current_is_requisition_wb;
+        next_is_requisition_wb  <= is_requisition_wb_i;
 
       when others =>
+
+        -- it is a new requisition. starts memory interface.
         if (is_requisition = '1' and waiting_for_memory = '0') then
           data_requisition        <= is_requisition;
           data_address            <= alu_result;
           data_write_enable       <= is_write and (is_requisition);
           data_write_data         <= is_write_data;
-          next_data_requisition   <= is_requisition;
-          next_data_address       <= alu_result;
-          next_data_write_enable  <= is_write and (is_requisition);
-          next_data_write_data    <= is_write_data;
+          data_requisition_i      <= is_requisition;
+          data_address_i          <= alu_result;
+          data_write_enable_i     <= is_write and (is_requisition);
+          data_write_data_i       <= is_write_data;
           next_waiting_for_memory <= not data_grant;
           next_is_requisition_wb  <= data_grant;
+        -- requisition happening and still waiting for memory to answer
         elsif (waiting_for_memory = '1') then
           data_requisition        <= last_data_requisition;
           data_address            <= last_data_address;
           data_write_enable       <= last_data_write_enable;
           data_write_data         <= last_data_write_data;
-          next_data_requisition   <= last_data_requisition;
-          next_data_address       <= last_data_address;
-          next_data_write_enable  <= last_data_write_enable;
-          next_data_write_data    <= last_data_write_data;
+          data_requisition_i      <= last_data_requisition;
+          data_address_i          <= last_data_address;
+          data_write_enable_i     <= last_data_write_enable;
+          data_write_data_i       <= last_data_write_data;
           next_waiting_for_memory <= not data_grant;
           next_is_requisition_wb  <= data_grant;
+        -- just a logical operation
         else
           data_requisition        <= '0';
           data_address            <= (others => '0');
           data_write_enable       <= '0';
           data_write_data         <= (others => '0');
-          next_data_requisition   <= '0';
-          next_data_address       <= (others => '0');
-          next_data_write_enable  <= '0';
-          next_data_write_data    <= (others => '0');
+          data_requisition_i      <= '0';
+          data_address_i          <= (others => '0');
+          data_write_enable_i     <= '0';
+          data_write_data_i       <= (others => '0');
           next_waiting_for_memory <= '0';
           next_is_requisition_wb  <= '0';
         end if;
+
     end case;
   end process combinational;
 
