2555ae37c20ca8c171969c60eda0a76b541c3bff
Matheus Cavalcante <matheus.cavalcante@ee.ufcg.edu.br>
2017-03-19 22:02:51 +0100

    Seems to work...

--- vhd/general_purpose_registers.vhd
+++ vhd/general_purpose_registers.vhd
@@ -51,17 +51,9 @@ begin  -- architecture behavioural
   begin  -- process seq_process
 
     if rst_n = '0' then                 -- asynchronous reset (active low)
-      -- clears outputs
-     read_data_a <= (others => '0');
-     read_data_b <= (others => '0');
-
       -- clears register bank
       gpr <= (others => (others => '0'));
     elsif clk'event and clk = '1' then  -- rising clock edge
-      -- clears outputs
-     read_data_a <= next_read_data_a;
-     read_data_b <= next_read_data_b;
-
       -- rewrites specific address in register bank
       -- can not rewrite register r0
       if (write_enable_z = '1' and (write_address_z /= "00000")) then
@@ -85,8 +77,8 @@ begin  -- architecture behavioural
   -- outputs: next_read_data_a, next_read_data_b
   comb_proc : process (gpr, read_address_a, read_address_b) is
   begin  -- process comb_proc
-    next_read_data_a <= gpr(to_integer(unsigned(read_address_a)));
-    next_read_data_b <= gpr(to_integer(unsigned(read_address_b)));
+    read_data_a <= gpr(to_integer(unsigned(read_address_a)));
+    read_data_b <= gpr(to_integer(unsigned(read_address_b)));
   end process comb_proc;
 
 end architecture behavioural;
