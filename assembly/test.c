1c5e097fc6188c2664c49dbe813b27a7f2a00948
Matheus Cavalcante <matheus.cavalcante@ee.ufcg.edu.br>
2017-04-02 12:52:14 +0200

    fixing errors?

--- assembly/test.c
+++ assembly/test.c
@@ -1,18 +1,11 @@
-#define HEX_ADDR 0x3406c
-#define IO_ADDR  0x34070
-
-int *hex = (int*) HEX_ADDR;
-int *io  = (int*) IO_ADDR;
-
-void print(int n) { *hex = n; }
-void put(int n) { *io = n; }
-int get(void) { return *io; }
+#include <math.h>
+#include "lib_phelmino.h"
 
 int main(void) {
     int a = 5;
-    int b = 8;
+    int b = 7;
 
-    put(a << b);
-
+    print(b / a);
+    
     return 0;
 }
